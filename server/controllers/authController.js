a22d7a9cc0776c824e3253c46ffea948fdca4cc4
noelmarior <noelroche06@gmail.com>
2026-02-21 17:43:58 +0530

    fix: error with smtp in deployment

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -161,7 +161,9 @@ exports.forgotPassword = async (req, res) => {
 
     try {
       const transporter = nodemailer.createTransport({
-        service: process.env.SMTP_SERVICE,
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: {
           user: process.env.SMTP_MAIL,
           pass: process.env.SMTP_PASSWORD,
